93276781bc2cdd74458ca8d7888960086378cde2
Kunal Nasa <111643119+KunalNasa@users.noreply.github.com>
2023-05-15 02:53:27 -0700

    Add files via upload

---
+++ tut.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char arr[10];
+    cout<<"Enter your name: ";
+    cin>>arr;
+    cout<<"hello cpp"<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<arr[i];
+    }
+    
+    return 0;
+}
